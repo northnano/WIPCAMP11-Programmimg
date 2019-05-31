94e8cd8995f5467810ea8552b3672835556a96da
northnano <51054730+northnano@users.noreply.github.com>
2019-05-31 11:19:55 +0700

    ProjectWIP

---
+++ WIP-XI/src/XI/Project/TEST.java
@@ -0,0 +1,51 @@
+/*
+ * WIP XI Computer Programing 
+ */
+package XI.Project;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author sittiwatlcp
+ */
+public class WIPCargo {
+    public static void main(String[] args) {
+        int item = 0;
+        int limit = 10;
+        int menu = 0;
+        while (limit=10){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1 to Add");
+        System.out.println("2 to Remove");
+        System.out.println("3 to Check the amount");
+        System.out.print("Choose your action : ");
+        menu = sc.nextInt();
+        if (menu == 1) {
+            int amountAdd = 0;
+            System.out.print("How many will you add : ");
+            amountAdd = sc.nextInt();
+            if (item + amountAdd <= limit) {
+                item = amountAdd + item;
+                System.out.println("Your amount of item in cago is : " + item);
+            } else {
+                System.out.println("โกดังเต็มแล้ว ไม่สามารถเพิ่มอีกได้");
+            }
+        }
+        if (menu == 2) {
+            int amountDel;
+            System.out.print("How many will you take out : ");
+            amountDel = sc.nextInt();
+            if (item - amountDel >= 0) {
+                item = item - amountDel;
+                System.out.println("Your amount of item in cago is : " + item);
+            } else {
+                System.out.println("โกดังมีน้อยกว่าที่คุณต้องการจะเอาออก");
+            }break;
+        }
+        if (menu == 3) {
+            System.out.println("Your amount of item in cago is : " + item);
+        }break;
+        }
+    }
+    }

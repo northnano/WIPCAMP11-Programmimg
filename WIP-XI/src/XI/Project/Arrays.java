136b3962258e05c1bf84bd4c2e799a3036e64ba0
northnano <51054730+northnano@users.noreply.github.com>
2019-05-31 11:43:47 +0700

    ProjectWIP

---
+++ WIP-XI/src/XI/Project/Arrays.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.Arrays;
+import java.util.Scanner;
+package WIP-XI;
+
+/**
+ *
+ * @author WipCamp11
+ */
+public class Arrays {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+        int Matrix[9];
+        Matrix = new int[i]
+                
+//        for(int i = 0; i < matrix.length; i++) 
+//        {
+ //           System.out.println(matrix[i]);
+   //     }
+        
+        
+        
+        // TODO code application logic here
+    }
+    
+}
